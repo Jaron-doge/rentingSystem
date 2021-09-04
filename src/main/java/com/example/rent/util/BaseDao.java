dda7394741f5990e5989de9d6fce89ab9ce35cb1
Jaron <i5363708miejiao92@163.com>
2021-09-04 10:19:56 +0800

    优化数据库连接方式改为连接池方式
    1. 将druid jar包导入
    2. 将resources文件夹改为resources文件夹
      File-> Project Structure -> Modules -> 将resources文件夹设置为resources
    3. 在BaseDao中添加需要的包
    4. 成功连接后可以将注释的代码删掉

--- src/main/java/com/example/rent/util/BaseDao.java
+++ src/main/java/com/example/rent/util/BaseDao.java
@@ -5,19 +5,50 @@ import java.sql.*;
 
 public class BaseDao {
 
-    private static String driver = "com.mysql.jdbc.Driver";
-    private static String url = "jdbc:mysql://bj-cynosdbmysql-grp-n1ox8dpy.sql.tencentcdb.com:23867/rentingSystem?useUnicode=true&characterEncoding=utf-8";
-    private static String user = "admin";
-    private static String password = "Ad123456";
+    // private static String driver = "com.mysql.jdbc.Driver";
+    // private static String url = "jdbc:mysql://bj-cynosdbmysql-grp-n1ox8dpy.sql.tencentcdb.com:23867/rentingSystem?useUnicode=true&characterEncoding=utf-8";
+    // private static String user = "admin";
+    // private static String password = "Ad123456";
+
+    // /**
+    //  * 连接数据库的方法n
+    //  */
+    // public static Connection getConnection() throws ClassNotFoundException, SQLException {
+    //     // 加载数据库驱动
+    //     Class.forName(driver);
+    //     Connection con = DriverManager.getConnection(url,user,password);
+    //     return con;
+    // }
+
+
+    private static DruidDataSource dataSource;
+    static {
+        try {
+            Properties properties = new Properties();
+            // 读取druid.properties 属性配置文件
+            InputStream inputStream =
+                    ClassLoader.getSystemClassLoader().getResourceAsStream("druid.properties");
+            // 从流中加载数据
+            properties.load(inputStream);
+            // 创建数据库连接池
+            dataSource = (DruidDataSource) DruidDataSourceFactory.createDataSource(properties);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     /**
-     * 连接数据库的方法n
+     * 获取数据库连接池中的连接
+     * @return 如果返回null,说明获取连接失败，有值就是获取连接成功
      */
-    public static Connection getConnection() throws ClassNotFoundException, SQLException {
-        // 加载数据库驱动
-        Class.forName(driver);
-        Connection con = DriverManager.getConnection(url,user,password);
-        return con;
+    public static Connection getConnection(){
+        Connection conn = null;
+        try {
+            conn = dataSource.getConnection();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return conn;
     }
 
     /**
